833d275807e3660bc5e095197270c794e807da94
McKayla Lankau <lankaukk@yahoo.com>
2020-08-26 03:21:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
-# code here!
+class School 
+  def initialize(name)
+    @name = name
+    roster = {}
+  end
+  
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def roster
+    roster
+  end
+  
+  def add_student(name, grade)
+    if roster[:grade]
+      roster[:grade] << name
+    elsif roster = {}
+      roster[:grade] = []
+      roster[:grade] << name
+    end
+  end
+  
+  def grade(grade)
+    return roster[grade]
+  end
+  
+  def sort
+    return roster.sort
+  end
+  
+end
+
+
+
